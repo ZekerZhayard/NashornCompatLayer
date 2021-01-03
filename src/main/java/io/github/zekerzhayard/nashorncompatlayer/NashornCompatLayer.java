2adb3b8aea69917a0a09f3e0588e0ab9333c8529
ZekerZhayard <trees1000@qq.com>
2021-01-03 23:33:09 +0800

    Update license to 2021.

--- src/main/java/io/github/zekerzhayard/nashorncompatlayer/NashornCompatLayer.java
+++ src/main/java/io/github/zekerzhayard/nashorncompatlayer/NashornCompatLayer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020  ZekerZhayard
+ * Copyright (C) 2020-2021  ZekerZhayard
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
